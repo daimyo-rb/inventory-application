d93a91c8c3f4d26ef3e77c68011988f8420cd6af
daimyo-rb <daimyo@roshibros.com>
2025-08-07 22:57:05 -0500

    Add form validation using express-validator

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,33 +1,52 @@
 const db = require('../db/queries');
+const { body, validationResult } = require("express-validator");
 
 async function getCategory(req, res) {
   const category_id = req.params.category_id
   const rows = await db.getItemsByCategoryId(category_id);
   const category = await db.getCategoryById(category_id)
-  console.log(category);
   res.render('category', {category: category, items: rows});
 }
 
 async function getCategoryUpdate(req, res) {
-  console.log(req.params.category_id)
   const category = await db.getCategoryById(req.params.category_id);
-  console.log(category)
   res.render('editCategory', {category: category});
 }
 
-async function postCategoryUpdate(req, res) {
-  await db.updateCategoryById(req.body.category_name, req.body.category_id);
-  res.redirect('/');
-}
+const postCategoryUpdate = [
+  body('category_name').trim()
+    .isLength({min: 1, max: 50}).withMessage('name must be between 1 and 50 characters.'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('editCategory',
+        {category: {id: req.body.category_id,
+          category_name: req.body.category_name},
+          errors: errors.array()
+        });
+    }
+    await db.updateCategoryById(req.body.category_name, req.body.category_id);
+    res.redirect('/');
+  }
+]
 
 function getNewCategory(req, res) {
   res.render('newCategory');
 }
 
-async function postNewCategory(req, res) {
-  await db.createNewCategory(req.body.category_name);
-  res.redirect('/');
-}
+const postNewCategory = [
+  body('category_name').trim()
+    .isLength({min: 1, max: 50}).withMessage('name must be between 1 and 50 characters.'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('newCategory', {errors: errors.array()});
+    }
+    await db.createNewCategory(req.body.category_name);
+    res.redirect('/');
+  }
+]
+
 
 async function deleteCategory(req, res) {
   const category_id = req.params.category_id;
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,24 +1,57 @@
 const db = require('../db/queries');
+const { body, validationResult } = require("express-validator");
 
 async function getItemUpdate(req, res) {
   const item = await db.getItemById(req.params.id);
   res.render('item', {item: item});
 }
 
-async function postItemUpdate(req, res) {
-  await db.updateItemById(req.params.id, req.body.item_name, req.body.item_qty, req.body.item_category);
-  res.redirect('/');
-}
+const postItemUpdate = [
+  body('item_name').trim()
+    .isLength({min: 1, max: 50}).withMessage('item name must be between 1 and 50 characters'),
+  body('item_qty').trim()
+    .isInt({min: 0}).withMessage('item quantity must be an integer >= 0'),
+  body('item_category').trim()
+    .isInt({min: 0}).withMessage('category must be integer >= 0; todo: make this one of the values in categories table'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('item', {
+        item: {id: req.params.id,
+          item_name: req.body.item_name,
+          item_qty: req.body.item_qty,
+          category_id: req.body.item_category
+        },
+        errors: errors.array()
+      });
+    }
+    await db.updateItemById(req.params.id, req.body.item_name, req.body.item_qty, req.body.item_category);
+    res.redirect('/');
+  }
+
+]
+
 
 function getNewItem(req, res) {
   const category_id = req.query.category_id;
   res.render('newItem', {category_id: category_id});
 }
 
-async function postNewItem(req, res) {
-  await db.createNewItem(req.body.item_name, req.body.item_qty, req.body.category_id);
-  res.redirect('/');
-}
+const postNewItem = [
+  body('item_name').trim()
+    .isLength({min: 1, max: 50}).withMessage('Item name must be between 1 and 50 characters'),
+  body('item_qty').trim()
+    .isInt({min: 0}).withMessage('item quantity must be an integer >= 0'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('newItem', {category_id: req.body.category_id,
+        errors: errors.array()});
+    }
+    await db.createNewItem(req.body.item_name, req.body.item_qty, req.body.category_id);
+    res.redirect('/');
+  }
+]
 
 async function deleteItem(req, res) {
   await db.deleteItemById(req.params.id);
